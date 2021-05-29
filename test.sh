f61088613d6acf024a4cc600ca4e5dec785ea5a9
Runxuan Jiang <29605531+runxuanjiang@users.noreply.github.com>
2021-05-29 14:23:03 -0400

    finished analysis testing

---
+++ test.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# The interpreter used to execute the script
+
+#“#SBATCH” directives that convey submission options:
+
+#SBATCH --job-name=conformer-rl-test
+#SBATCH --mail-user=runxuanj@umich.edu
+#SBATCH --cpus-per-task=1
+#SBATCH --nodes=1
+#SBATCH --time=2:00:00
+#SBATCH --account=tewaria1
+#SBATCH --partition=standard
+#SBATCH --output=/home/%u/%x-%j.log
+#SBATCH --get-user-env
+
+# The application(s) to execute along with its input arguments and options:
+conda activate my-rdkit-env
+python -u test.py
